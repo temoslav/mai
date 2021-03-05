f6473c68b662a84344985f1c6efb3da94bb4fd13
temoslav <73434222+temoslav@users.noreply.github.com>
2021-03-06 00:12:33 +0300

    Add files via upload

---
+++ Source5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    int sum = 0;
+    int x;
+    cout << "put kolvo" << endl;
+    cin >> x;
+    char simbol;
+    for (int i = 0; i < x; ++i) {
+        cin >> simbol;
+        if ((simbol >= '0') && (simbol <= '9')) {
+            sum += (int)simbol - 48;
+        }
+        else {
+            cout << (char)tolower(simbol);
+        }
+
+    }
+    cout << "sum= " << sum << endl;
+    return 0;
+}
